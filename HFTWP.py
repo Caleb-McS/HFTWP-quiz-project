6f5fb7b80185eb6636dc71c4768a6cf2c14bb574
Caleb-McS <mcseveneyc@stu.otc.school.nz>
2026-05-01 09:57:35 +1200

    More questions

--- HFTWP.py
+++ HFTWP.py
@@ -1,7 +1,8 @@
 """FINISH THIS LATERER IF NOT ME FAIL AND CRY"""
 import os
 os.system('cls')
-ques = ["All of the following a characters in the film except...", "Who did Ricky name his dog after?", "What animal killed Zag"]
-awns = ["1: Ricky Baker\n2: Bella Faulkner\n3: Paula Hall\n4: Trisha Smith", "1: Tupac Shakur\n2: John Wick\n3: Mortron\n4: Zig", "1: A tic\n2: A boar\n3: A snake\n4: A "]
-for i in ques:
-    print(i)
+orde = ["0","1","2","3","4","5","6","7"]
+ques = ["All of the following a characters in the film except...", "Who did Ricky name his dog after?", "What animal killed Zag", "Who can't read or write?"]
+awns = ["1: Ricky Baker\n2: Bella Faulkner\n3: Paula Hall\n4: Trisha Smith", "1: Tupac Shakur\n2: John Wick\n3: Mortron\n4: Zig", "1: A tic\n2: A boar\n3: A snake\n4: A hunter", "1: Uncle Hec\n2: Psyco Sam\n3: Ricky Baker\n4: Kahu"]
+for i in awns:
+    print(f"")
