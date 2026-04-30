d07cde9f9afa3c9cebe701aca33f9fd2b783263b
Caleb-McS <mcseveneyc@stu.otc.school.nz>
2026-05-01 09:13:15 +1200

    3 questions

--- HFTWP.py
+++ HFTWP.py
@@ -1,4 +1,7 @@
 """FINISH THIS LATERER IF NOT ME FAIL AND CRY"""
 import os
-ques = ["All of the following a characters in the film except..."]
-awns = ["1: Ricky Baker \n2: Bella Faulkner \n3: Paula Hall \n4: Trisha Smith"]
+os.system('cls')
+ques = ["All of the following a characters in the film except...", "Who did Ricky name his dog after?", "What animal killed Zag"]
+awns = ["1: Ricky Baker\n2: Bella Faulkner\n3: Paula Hall\n4: Trisha Smith", "1: Tupac Shakur\n2: John Wick\n3: Mortron\n4: Zig", "1: A tic\n2: A boar\n3: A snake\n4: A "]
+for i in ques:
+    print(i)
