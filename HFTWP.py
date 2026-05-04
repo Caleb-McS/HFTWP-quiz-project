7fdc4d61b63e0a2c78bebc7a2f1409b8aaa4b0c7
Caleb-McS <mcseveneyc@stu.otc.school.nz>
2026-05-05 10:58:50 +1200

    finished questions

--- HFTWP.py
+++ HFTWP.py
@@ -2,7 +2,10 @@
 import os
 os.system('cls')
 orde = ["0","1","2","3","4","5","6","7"]
-ques = ["All of the following a characters in the film except...", "Who did Ricky name his dog after?", "What animal killed Zag", "Who can't read or write?", "What does Bella put in ricky's bed to cheer him up?", ""]
-awns = ["1: Ricky Baker\n2: Bella Faulkner\n3: Paula Hall\n4: Trisha Smith", "1: Tupac Shakur\n2: John Wick\n3: Mortron\n4: Zig", "1: A tic\n2: A boar\n3: A snake\n4: A hunter", "1: Uncle Hec\n2: Psyco Sam\n3: Ricky Baker\n4: Kahu", "1: A bear trap \n2: A hot water bottle\n3: An apple\n4: A car", ""]
-for i in awns:
-    print(f"")
+ques = {
+    "All of the following characters are in the film except..."
+    "Who did Ricky name his dog after?"
+    "What animal killed Zag", "Who can't read or write?", "What does Bella put in ricky's bed to cheer him up?", "How many household did ricky go trough?", "How many dogs died in the film?", "How many cats appeared in the film?"}
+awns = ["1: Ricky Baker\n2: Bella Faulkner\n3: Paula Hall\n4: Trisha Smith", "1: Tupac Shakur\n2: John Wick\n3: Mortron\n4: Zig", "1: A tic\n2: A boar\n3: A snake\n4: A hunter", "1: Uncle Hec\n2: Psyco Sam\n3: Ricky Baker\n4: Kahu", "1: A bear trap \n2: A hot water bottle\n3: An apple\n4: A car", "1: 1\n2:2\n3:3\n4: Too many to count", "1: 0\n2: 1\n3: 2\n4: 3", "1: 0\n2: 1\n3: 2\n4: 3"]
+for i in ques:
+    print(i)
