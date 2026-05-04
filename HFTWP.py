9e539bad94a86f0eb5d0d3a14e27c185e353d64d
Caleb-McS <mcseveneyc@stu.otc.school.nz>
2026-05-04 13:27:22 +1200

    added more questions

--- HFTWP.py
+++ HFTWP.py
@@ -2,7 +2,7 @@
 import os
 os.system('cls')
 orde = ["0","1","2","3","4","5","6","7"]
-ques = ["All of the following a characters in the film except...", "Who did Ricky name his dog after?", "What animal killed Zag", "Who can't read or write?"]
-awns = ["1: Ricky Baker\n2: Bella Faulkner\n3: Paula Hall\n4: Trisha Smith", "1: Tupac Shakur\n2: John Wick\n3: Mortron\n4: Zig", "1: A tic\n2: A boar\n3: A snake\n4: A hunter", "1: Uncle Hec\n2: Psyco Sam\n3: Ricky Baker\n4: Kahu"]
+ques = ["All of the following a characters in the film except...", "Who did Ricky name his dog after?", "What animal killed Zag", "Who can't read or write?", "What does Bella put in ricky's bed to cheer him up?", ""]
+awns = ["1: Ricky Baker\n2: Bella Faulkner\n3: Paula Hall\n4: Trisha Smith", "1: Tupac Shakur\n2: John Wick\n3: Mortron\n4: Zig", "1: A tic\n2: A boar\n3: A snake\n4: A hunter", "1: Uncle Hec\n2: Psyco Sam\n3: Ricky Baker\n4: Kahu", "1: A bear trap \n2: A hot water bottle\n3: An apple\n4: A car", ""]
 for i in awns:
     print(f"")
