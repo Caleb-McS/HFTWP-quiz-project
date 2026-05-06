05a285822b70faf7c2a8e7c91a4a21dc522e2aa3
Caleb-McS <mcseveneyc@stu.otc.school.nz>
2026-05-06 15:14:52 +1200

    Added first question code

--- HFTWP.py
+++ HFTWP.py
@@ -1,11 +1,34 @@
 """FINISH THIS LATERER IF NOT ME FAIL AND CRY"""
 import os
 os.system('cls')
+amo = 0
 orde = ["0","1","2","3","4","5","6","7"]
+opt = ["1", "2", "3", "4"]
 ques = {
-    "All of the following characters are in the film except..."
-    "Who did Ricky name his dog after?"
-    "What animal killed Zag", "Who can't read or write?", "What does Bella put in ricky's bed to cheer him up?", "How many household did ricky go trough?", "How many dogs died in the film?", "How many cats appeared in the film?"}
-awns = ["1: Ricky Baker\n2: Bella Faulkner\n3: Paula Hall\n4: Trisha Smith", "1: Tupac Shakur\n2: John Wick\n3: Mortron\n4: Zig", "1: A tic\n2: A boar\n3: A snake\n4: A hunter", "1: Uncle Hec\n2: Psyco Sam\n3: Ricky Baker\n4: Kahu", "1: A bear trap \n2: A hot water bottle\n3: An apple\n4: A car", "1: 1\n2:2\n3:3\n4: Too many to count", "1: 0\n2: 1\n3: 2\n4: 3", "1: 0\n2: 1\n3: 2\n4: 3"]
+    "All of the following characters are in the film except..." : 4,
+    "Who did Ricky name his dog after?" : 1,
+    "What animal killed Zag" : 2,
+    "Who can't read or write?" : 1,
+    "What does Bella put in ricky's bed to cheer him up?" : 2,
+    "How many household did ricky go trough?" : 4,
+    "How many dogs died in the film?" : 2,
+    "How many cats appeared in the film?" : 1
+}
+awns = [
+    "1: Ricky Baker\n2: Bella Faulkner\n3: Paula Hall\n4: Trisha Smith", 
+    "1: Tupac Shakur\n2: John Wick\n3: Mortron\n4: Zig",
+    "1: A tic\n2: A boar\n3: A snake\n4: A hunter", "1: Uncle Hec\n2: Psyco Sam\n3: Ricky Baker\n4: Kahu", 
+    "1: A bear trap \n2: A hot water bottle\n3: An apple\n4: A car", 
+    "1: 1\n2: 2\n3: 3\n4: Too many to count", 
+    "1: 0\n2: 1\n3: 2\n4: 3", 
+    "1: 0\n2: 1\n3: 2\n4: 3"
+]
 for i in ques:
     print(i)
+    print(awns[amo])
+    amo += 1
+    awnse = int(input(""))
+    if awnse in opt:
+        print(hmm)
+    else:
+        print("invalid input")
